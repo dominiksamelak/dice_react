648c09391a51e161e559b4c0613fb20cae0b69b3
Damian Bachórz <damian.bachorz@hotmail.com>
2023-12-05 00:07:07 +0100

    set display block to dice img

--- src/Die.js
+++ src/Die.js
@@ -8,7 +8,7 @@ export default function Die(props) {
   };
   return (
     <div className="die-face" onClick={props.holdDice} style={styles}>
-      <img src={`img/${props.src}`} alt="dice" />
+      <img src={`img/${props.src}`} alt="dice" className="dice-img" />
     </div>
   );
 }
