da484c5220457c4d73da9d55358b2d2b55c2002a
HarryDSDS <dominiksamelak@gmail.com>
2023-12-04 23:33:14 +0100

    dice_react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Die from "./Die"
+import {nanoid} from "nanoid"
+import "./dice.css"
+// import oneDie from './img/1.png'
+// import twoDice from './img/2.png'
+// import threeDice from './img/3.png'
+// import fourDice from './img/4.png'
+// import fiveDice from './img/5.png'
+// import sixDice from './img/6.png'
+
 
 function App() {
+  const [dice, setDice] = React.useState(newRoll())
+
+  function generateNewDie() {
+    const diceValue = Math.ceil(Math.random() * 6)
+    return {
+        value: diceValue,
+        isHeld: false,
+        id: nanoid(),
+        src: `${diceValue}.png`
+    }
+}
+
+  function newRoll() {
+    const newDice = []
+    for (let i=0; i< 5; i++){
+      newDice.push(generateNewDie())
+    }
+    return newDice
+
+  }
+
+  function rollUnSelected() {
+    setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ? 
+            die :
+            generateNewDie()
+    }))
+}
+
+  function rollAll() {
+    setDice(newRoll())
+  }
+  
+function holdDice(id) {
+  setDice(oldDice => oldDice.map(die => {
+      return die.id === id ? 
+          {...die, isHeld: !die.isHeld} :
+          die
+  }))
+}
+
+  const diceElements = dice.map(die => (
+    <Die 
+    key={die.id} 
+    value={die.value} 
+    isHeld={die.isHeld} 
+    src={die.src} 
+    holdDice={() => holdDice(die.id)}
+    />
+))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+    <div className="dice-container">
+      {diceElements}
     </div>
+    <button onClick={rollAll}>Roll all</button>
+    <button onClick={rollUnSelected}>Roll unselected</button>
+    </main>
+
+
   );
 }
 
