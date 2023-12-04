fe5ffa1f0c0fef3364b32021227002c73939dfd4
Damian Bachórz <damian.bachorz@hotmail.com>
2023-12-04 23:53:59 +0100

    remove newRoll, move to generateDice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Die from "./Die"
-import {nanoid} from "nanoid"
-import "./dice.css"
+import React from "react";
+import Die from "./Die";
+import { nanoid } from "nanoid";
+import "./dice.css";
 // import oneDie from './img/1.png'
 // import twoDice from './img/2.png'
 // import threeDice from './img/3.png'
@@ -9,69 +9,57 @@ import "./dice.css"
 // import fiveDice from './img/5.png'
 // import sixDice from './img/6.png'
 
-
 function App() {
-  const [dice, setDice] = React.useState(newRoll())
+  const [dice, setDice] = React.useState(generateNewDie());
 
   function generateNewDie() {
-    const diceValue = Math.ceil(Math.random() * 6)
-    return {
-        value: diceValue,
-        isHeld: false,
-        id: nanoid(),
-        src: `${diceValue}.png`
-    }
-}
-
-  function newRoll() {
-    const newDice = []
-    for (let i=0; i< 5; i++){
-      newDice.push(generateNewDie())
-    }
-    return newDice
-
+    return Array(5)
+      .fill()
+      .map(() => {
+        const diceValue = Math.ceil(Math.random() * 6);
+        return {
+          value: diceValue,
+          isHeld: false,
+          id: nanoid(),
+          src: `${diceValue}.png`,
+        };
+      });
   }
 
   function rollUnSelected() {
-    setDice(oldDice => oldDice.map(die => {
-        return die.isHeld ? 
-            die :
-            generateNewDie()
-    }))
-}
+    setDice((oldDice) =>
+      oldDice.map((die) => (die.isHeld ? die : generateNewDie()))
+    );
+  }
 
   function rollAll() {
-    setDice(newRoll())
+    setDice(generateNewDie());
   }
-  
-function holdDice(id) {
-  setDice(oldDice => oldDice.map(die => {
-      return die.id === id ? 
-          {...die, isHeld: !die.isHeld} :
-          die
-  }))
-}
 
-  const diceElements = dice.map(die => (
-    <Die 
-    key={die.id} 
-    value={die.value} 
-    isHeld={die.isHeld} 
-    src={die.src} 
-    holdDice={() => holdDice(die.id)}
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      src={die.src}
+      holdDice={() => holdDice(die.id)}
     />
-))
+  ));
 
   return (
     <main>
-    <div className="dice-container">
-      {diceElements}
-    </div>
-    <button onClick={rollAll}>Roll all</button>
-    <button onClick={rollUnSelected}>Roll unselected</button>
+      <div className="dice-container">{diceElements}</div>
+      <button onClick={rollAll}>Roll all</button>
+      <button onClick={rollUnSelected}>Roll unselected</button>
     </main>
-
-
   );
 }
 
