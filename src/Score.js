9a50e69a1c95a7152ee63e6767a3c3cdb7852d59
Damian Bachórz <damian.bachorz@hotmail.com>
2023-12-05 21:39:05 +0100

    fix

--- src/Score.js
+++ src/Score.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function Score(props) {
+  return (
     <section class="player-one-score">
-        <div class="school">
-            <div class="one-container">
-                <p id="one">1</p>
-                <div class="school-one">---</div>
-                <button class="confirm-one" id="button-school">Pick</button>
-            </div>
+      <div class="school">
+        <div class="one-container">
+          <p id="one">1</p>
+          <div class="school-one">---</div>
+          <button class="confirm-one" id="button-school">
+            Pick
+          </button>
         </div>
+      </div>
     </section>
+  );
 }
