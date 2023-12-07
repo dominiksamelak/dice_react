50da9e91a939f99713ab3da25816c2ad05b22e95
HarryDSDS <dominiksamelak@gmail.com>
2023-12-07 23:14:50 +0100

    Added ability to count score based on dice thrown

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ import "./dice.css";
 import Score from "./Score";
 
 const DICE_COUNT = 5;
+let oneValue = 0
 
 function App() {
   const [dice, setDice] = React.useState(newRoll());
-  const [countOne, setCountOne] = React.useState();
-
+  const [scoreOne, setScoreOne] = React.useState("---");
+  const [scoreTwo, setScoreTwo] = React.useState("---");
+  const [scoreThree, setScoreThree] = React.useState("---");
+  const [scoreFour, setScoreFour] = React.useState("---");
+  const [scoreFive, setScoreFive] = React.useState("---");
+  const [scoreSix, setScoreSix] = React.useState("---");
+  
   function generateNewDie() {
     const diceValue = Math.floor(Math.random() * DICE_COUNT) + 1;
     return {
@@ -44,9 +50,104 @@ function App() {
   }
 
   useEffect(() => {
-    setCountOne(dice.filter((die) => die.value === 1).length);
-    // console.log(countOne)
-  }, [dice, setCountOne]);
+    const countOneValue = dice.filter((die) => die.value === 1).length;
+    const countTwoValue = dice.filter((die) => die.value === 2).length;
+    const countThreeValue = dice.filter((die) => die.value === 3).length;
+    const countFourValue = dice.filter((die) => die.value === 4).length;
+    const countFiveValue = dice.filter((die) => die.value === 5).length;
+    const countSixValue = dice.filter((die) => die.value === 6).length;
+
+    // Define scoring logic for value 1
+    if (countOneValue === 1) {
+      setScoreOne(-2);
+    } else if (countOneValue === 2) {
+      setScoreOne(-1);
+    } else if (countOneValue === 3) {
+      setScoreOne(0);
+    } else if (countOneValue === 4) {
+      setScoreOne(1);
+    } else if (countOneValue === 5) {
+      setScoreOne(2);
+    } else {
+      setScoreOne("---");
+    }
+
+    // Define scoring logic for value 2
+    if (countTwoValue === 1) {
+      setScoreTwo(-4);
+    } else if (countTwoValue === 2) {
+      setScoreTwo(-2);
+    } else if (countTwoValue === 3) {
+      setScoreTwo(0);
+    } else if (countTwoValue === 4) {
+      setScoreTwo(2);
+    } else if (countTwoValue === 5) {
+      setScoreTwo(4);
+    } else {
+      setScoreTwo("---");
+    }
+
+    // Define scoring logic for value 3
+    if (countThreeValue === 1) {
+      setScoreThree(-6);
+    } else if (countThreeValue === 2) {
+      setScoreThree(-3);
+    } else if (countThreeValue === 3) {
+      setScoreThree(0);
+    } else if (countThreeValue === 4) {
+      setScoreThree(3);
+    } else if (countThreeValue === 5) {
+      setScoreThree(6);
+    } else {
+      setScoreThree("---");
+    }
+
+    // Define scoring logic for value 4
+    if (countFourValue === 1) {
+      setScoreFour(-8);
+    } else if (countFourValue === 2) {
+      setScoreFour(-4);
+    } else if (countFourValue === 3) {
+      setScoreFour(0);
+    } else if (countFourValue === 4) {
+      setScoreFour(4);
+    } else if (countFourValue === 5) {
+      setScoreFour(8);
+    } else {
+      setScoreFour("---");
+    }
+
+    // Define scoring logic for value 5
+    if (countFiveValue === 1) {
+      setScoreFive(-10);
+    } else if (countFiveValue === 2) {
+      setScoreFive(-5);
+    } else if (countFiveValue === 3) {
+      setScoreFive(0);
+    } else if (countFiveValue === 4) {
+      setScoreFive(5);
+    } else if (countFiveValue === 5) {
+      setScoreFive(10);
+    } else {
+      setScoreFive("---");
+    }
+
+    // Define scoring logic for value 5
+    if (countSixValue === 1) {
+      setScoreSix(-12);
+    } else if (countSixValue === 2) {
+      setScoreSix(-6);
+    } else if (countSixValue === 3) {
+      setScoreSix(0);
+    } else if (countSixValue === 4) {
+      setScoreSix(6);
+    } else if (countSixValue === 5) {
+      setScoreSix(12);
+    } else {
+      setScoreSix("---");
+    }
+  }, [dice]);
+
 
   const diceElements = dice.map((die) => (
     <Die
@@ -65,7 +166,14 @@ function App() {
       <button onClick={rollUnselected}>Roll unselected</button>
       {/* <button onClick={}>s</button> */}
       <div>
-        <Score countOne={countOne} />
+        <Score 
+        scoreOne={scoreOne}
+        scoreTwo={scoreTwo}
+        scoreThree={scoreThree}
+        scoreFour={scoreFour}
+        scoreFive={scoreFive}
+        scoreSix={scoreSix}
+         />
       </div>
     </main>
   );
--- src/Score.js
+++ src/Score.js
@@ -7,34 +7,34 @@ export default function Score(props) {
       <div className="school">
         <div className="one-container">
           <p id="one">1</p>
-          <div className="school-one">{props.countOneValue}</div>
+          <div className="school-one">{props.scoreOne}</div>
           <button className="confirm-one" id="button-school">
             Pick
           </button>
         </div>
         <div className="two-container">
           <p id="two">2</p>
-          <div className="school-two">{props.countTwoValue}</div>
+          <div className="school-two">{props.scoreTwo}</div>
           <button className="confirm-two" id="button-school">Pick</button>
         </div>
         <div className="three-container">
           <p id="three">3</p>
-          <div className="school-three">{props.countThreeValue}</div>
+          <div className="school-three">{props.scoreThree}</div>
           <button className="confirm-three" id="button-school">Pick</button>
         </div>
         <div className="four-container">
           <p id="four">4</p>
-          <div className="school-four">{props.countFourValue}</div>
+          <div className="school-four">{props.scoreFour}</div>
           <button className="confirm-four" id="button-school">Pick</button>
         </div>
         <div className="five-container">
           <p id="five">5</p>
-          <div className="school-five">{props.countFiveValue}</div>
+          <div className="school-five">{props.scoreFive}</div>
           <button className="confirm-five" id="button-school">Pick</button>
         </div>
         <div className="six-container">
           <p id="six">6</p>
-          <div className="school-six">{props.countSixValue}</div>
+          <div className="school-six">{props.scoreSix}</div>
           <button className="confirm-six" id="button-school">Pick</button>
         </div>
       </div>
