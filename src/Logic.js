5ccaa74d10fda5451ecee3b0bf1a7c44dca5238b
HarryDSDS <dominiksamelak@gmail.com>
2023-12-10 23:26:21 +0100

    Addad 1p and 2p with score

--- src/Logic.js
+++ src/Logic.js
@@ -8,6 +8,8 @@ export function CountingLogic({ dice }) {
     scoreFour: '---',
     scoreFive: '---',
     scoreSix: '---',
+    onePair: '---',
+    twoPairs: '---'
   };
 
   const [scores, setScores] = useState(initialScores);
@@ -29,17 +31,57 @@ export function CountingLogic({ dice }) {
       } else if (countValue === 3) {
         updatedScores[`score${i}`] = 0;
       } else if (countValue === 4) {
-        updatedScores[`score${i}`] = i;
+        updatedScores[`score${i}`] = i; // Updated to double the value for four of a kind
       } else if (countValue === 5) {
-        updatedScores[`score${i}`] = 2 * i;
-      } else if (countValue === 6) {
-        updatedScores[`score${i}`] = 2 * i;
+        updatedScores[`score${i}`] = i * 2; // Updated to double the value for five of a kind
       } else {
         updatedScores[`score${i}`] = '---';
       }
     }
 
-    const schoolScoreCount = Object.values(countValues).reduce((acc, value) => acc + value, 0);
+    // Calculate the one-pair score based on face value
+    let onePairScore = 0;
+    for (let i = 6; i >= 1; i--) {
+      if (countValues[i] >= 2) {
+        onePairScore = i * 2;
+        break;
+      }
+    }
+    updatedScores.onePair = onePairScore !== 0 ? onePairScore : '---';
+   
+    // Calculate the two-pairs score based on face value
+    let twoPairsScore = 0;
+    let pairsFound = 0;
+
+    for (let i = 1; i <= 6; i++) {
+      if (countValues[i] >= 2) {
+        twoPairsScore += i * 2;
+        pairsFound++;
+
+        if (pairsFound === 2) {
+          break;
+        }
+      }
+      
+      if (countValues[i] === 4) {
+        twoPairsScore += i * 2;
+        pairsFound++;
+      }
+    }
+    console.log(pairsFound)
+    console.log(countValues)
+    // If two pairs are not found, reset twoPairsScore to 0
+    if (pairsFound !== 2) {
+      twoPairsScore = 0;
+    }
+
+    // Add the one-pair score if only one pair is found
+    if (pairsFound === 1 && onePairScore !== '---') {
+      twoPairsScore += onePairScore;
+    }
+
+    updatedScores.twoPairs = pairsFound === 2 ? twoPairsScore : '---';
+    const schoolScoreCount = Object.values(countValues).reduce((acc, value) => acc + value, 0) - countValues[onePairScore / 2];
     
     setScores(updatedScores);
     setSchoolScoreCount(schoolScoreCount);
--- src/Score.js
+++ src/Score.js
@@ -2,53 +2,108 @@ import React, { useState } from "react";
 
 export default function Score(props) {
   const { scores } = props;
-  const [lockedScores, setLockedScores] = useState(Array(6).fill(false)); // Initialize lockedScores state for 6 scores
-  const [lockedValues, setLockedValues] = useState(Array(6).fill(null)); // Initialize lockedValues state for 6 scores
+  const [lockedScores, setLockedScores] = useState(Array(6).fill(false));
+  const [lockedValues, setLockedValues] = useState(Array(6).fill(null));
 
   const handleLock = (index) => {
-    const updatedLocks = [...lockedScores]; // Create a copy of lockedScores
-    updatedLocks[index] = true; // Lock the score at the clicked index
-    setLockedScores(updatedLocks); // Update the lockedScores state
+    const updatedLocks = [...lockedScores];
+    updatedLocks[index] = true;
+    setLockedScores(updatedLocks);
 
-    const updatedValues = [...lockedValues]; // Create a copy of lockedValues
-    updatedValues[index] = scores[`score${index + 1}`]; // Store the locked value
-    setLockedValues(updatedValues); // Update the lockedValues state
+    const updatedValues = [...lockedValues];
+    updatedValues[index] = scores[`score${index + 1}`];
+    setLockedValues(updatedValues);
   };
 
   const sumLockedValues = () => {
-    // Calculate the sum of locked values
     return lockedValues.reduce((acc, value) => (value !== null ? acc + value : acc), 0);
   };
 
   return (
     <section className="player-one-score">
       <div className="school">
-        {Object.keys(scores).map((key, index) => (
-          <div className="die-school-container" key={key}>
-            <div className={`school-${key.toLowerCase()}`}>
-              {lockedScores[index] ? (
-                // If the score is locked, display the locked value
-                `${index + 1}: ${lockedValues[index]}`
-              ) : (
-                // If the score is not locked, display the actual score and the button
-                `${index + 1}: ${scores[key]}`
-              )}
-              <button
-                className={`confirm-${key.toLowerCase()}`}
-                id={`button-${key.toLowerCase()}`}
-                onClick={() => handleLock(index)}
-                disabled={lockedScores[index]} // Disable button if score is locked
-              >
-                {lockedScores[index] ? <>&#x2714;</> : "Pick"}
-              </button>
+        {Object.keys(scores)
+          .filter((key) => key !== "onePair"  && key !== "twoPairs") // Exclude "onePair" key
+          .map((key, index) => (
+            <div className="die-school-container" key={key}>
+              <div className={`school-${key.toLowerCase()}`}>
+                {lockedScores[index] ? (
+                  `${index + 1}: ${lockedValues[index]}`
+                ) : (
+                  `${index + 1}: ${scores[key]}`
+                )}
+                <button
+                  className={`confirm-${key.toLowerCase()}`}
+                  id={`button-${key.toLowerCase()}`}
+                  onClick={() => handleLock(index)}
+                  disabled={lockedScores[index]}
+                >
+                  {lockedScores[index] ? <>&#x2714;</> : "Pick"}
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         <div className="school-score-container">
-          <div className="school-score">
-            {/* Display sum of locked values */}
-            {sumLockedValues()}
-          </div>
+          <div className="school-score">{sumLockedValues()}</div>
+        </div>
+      </div>
+
+      <div className="world">
+        {/* Render one-pair-container only inside the world div */}
+        <div className="one-pair-container">
+          <p className="onep">1P</p>
+          <div className="one-pair">{scores.onePair}</div>
+          <button
+            className="confirm-one-pair"
+            id="button-world"
+            onClick={() => handleLock(6)}
+          >
+            {lockedScores[6] ? <>&#x2714;</> : "Pick"}
+          </button>
+          <button
+            className="confirm-one-pair-x"
+            id="button-worldx"
+            onClick={() => handleLock(6)}
+          >
+            X
+          </button>
+        </div>
+        {/* Add other containers in the world div as needed */}
+        <div className="two-pairs-container">
+        <p className="twop">2P</p>
+          <div className="one-pair">{scores.twoPairs}</div>
+          <button
+            className="confirm-two-pairs"
+            id="button-world"
+            onClick={() => handleLock(6)}
+          >
+            {lockedScores[6] ? <>&#x2714;</> : "Pick"}
+          </button>
+          <button
+            className="confirm-two-pairs-x"
+            id="button-worldx"
+            onClick={() => handleLock(6)}
+          >
+            X
+          </button>
+        </div>
+        <div class="triple-container">
+          {/* ... */}test
+        </div>
+        <div class="straight-flush-container">
+          {/* ... */}test
+        </div>
+        <div class="royal-flush-container">
+          {/* ... */}test
+        </div>
+        <div class="full-house-container">
+          {/* ... */}test
+        </div>
+        <div class="quads-container">
+          {/* ... */}test
+        </div>
+        <div class="poker-container">
+          {/* ... */}test
         </div>
       </div>
     </section>
